f3ccc45122b8530bcea5192fc25f26f3043a5998
minijb <1414309848@qq.com>
2023-07-20 17:12:28 +0800

    add layers

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -22,11 +22,11 @@ namespace Hazel {
 	enum EventCategory
 	{
 		None = 0,
-		EventCategoryApplication    = BIT(0),
-		EventCategoryInput          = BIT(1),
-		EventCategoryKeyboard       = BIT(2),
-		EventCategoryMouse          = BIT(3),
-		EventCategoryMouseButton    = BIT(4)
+		EventCategoryApplication = BIT ( 0 ),
+		EventCategoryInput = BIT ( 1 ),
+		EventCategoryKeyboard = BIT ( 2 ),
+		EventCategoryMouse = BIT ( 3 ),
+		EventCategoryMouseButton = BIT ( 4 )
 	};
 
 #define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
@@ -37,37 +37,36 @@ namespace Hazel {
 
 	class HAZEL_API Event
 	{
-		friend class EventDispatcher;
 	public:
-		virtual EventType GetEventType() const = 0;
-		virtual const char* GetName() const = 0;
-		virtual int GetCategoryFlags() const = 0;
-		virtual std::string ToString() const { return GetName(); }
+		bool Handled = false;
 
-		inline bool IsInCategory(EventCategory category)
+		virtual EventType GetEventType ( ) const = 0;
+		virtual const char* GetName ( ) const = 0;
+		virtual int GetCategoryFlags ( ) const = 0;
+		virtual std::string ToString ( ) const { return GetName ( ); }
+
+		inline bool IsInCategory ( EventCategory category )
 		{
-			return GetCategoryFlags() & category;
+			return GetCategoryFlags ( ) & category;
 		}
-	protected:
-		bool m_Handled = false;
 	};
 
 	class EventDispatcher
 	{
 		template<typename T>
-		using EventFn = std::function<bool(T&)>;
+		using EventFn = std::function<bool ( T& )>;
 	public:
-		EventDispatcher(Event& event)
-			: m_Event(event)
+		EventDispatcher ( Event& event )
+			: m_Event ( event )
 		{
 		}
 
 		template<typename T>
-		bool Dispatch(EventFn<T> func)
+		bool Dispatch ( EventFn<T> func )
 		{
-			if (m_Event.GetEventType() == T::GetStaticType())
+			if ( m_Event.GetEventType ( ) == T::GetStaticType ( ) )
 			{
-				m_Event.m_Handled = func(*(T*)&m_Event);
+				m_Event.Handled = func ( *(T*)&m_Event );
 				return true;
 			}
 			return false;
@@ -76,9 +75,9 @@ namespace Hazel {
 		Event& m_Event;
 	};
 
-	inline std::ostream& operator<<(std::ostream& os, const Event& e)
+	inline std::ostream& operator<<( std::ostream& os, const Event& e )
 	{
-		return os << e.ToString();
+		return os << e.ToString ( );
 	}
 }
 
---
+++ Hazel/src/Hazel/Layer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Hazel/Core.h"
+#include "Hazel/Events/Event.h"
+
+namespace Hazel {
+
+	class HAZEL_API Layer
+	{
+	public:
+		Layer(const std::string& name = "Layer");
+		virtual ~Layer();
+
+		virtual void OnAttach() {}//当层被推送到栈上的时候
+		virtual void OnDetach() {}//当层被移除栈的时候
+		virtual void OnUpdate() {}
+		virtual void OnEvent(Event& event) {}
+
+		inline const std::string& GetName() const { return m_DebugName; }
+	protected:
+		std::string m_DebugName;
+	};
+
+}
